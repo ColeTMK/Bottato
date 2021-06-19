4aaa402a3c2a7f1d5f905b46f48aa8bd0c073550
Cole <82796357+ColeTMK@users.noreply.github.com>
2021-06-19 00:35:42 -0400

    Changed bot status

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def on_ready():
   channel = bot.get_channel(848722304069926993)
   print('The bot is online!')
   await bot.change_presence(
-    activity=discord.Activity(type=discord.ActivityType.watching, name="Youtube Videos"))
+    activity=discord.Activity(type=discord.ActivityType.watching, name="For >help :D"))
   print('Bot is connected to:')
   for server in bot.guilds:
     print(server.name, server.id)
